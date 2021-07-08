3bd2224cd09bf0b41a98a98301027d1ca5329b52
Loc Le <locle.isme@gmail.com>
2021-07-08 16:30:13 +0700

    add link support

--- frontend/src/components/VHeader.vue
+++ frontend/src/components/VHeader.vue
@@ -1,12 +1,17 @@
 <template>
-  <header>
+  <header class="header">
     <!--Navbar-->
 
-    <nav class="navbar navbar-light bg-light">
+    <nav class="navbar navbar-light bg-ut">
       <div class="container-fluid">
-        <div class="navbar-brand home" @click="refresh()">
+        <div class="navbar-brand home text-light" @click="refresh()">
           <i class="bi bi-house-door"></i>
           Calculator GPA
+        </div>
+        <div class="d-flex">
+          <a href="https://www.facebook.com/LocLe.isme" target="_blank"
+             class="btn btn-outline-dark me-2 text-light" type="button">
+            <i class="fa fa-support"></i> Hổ trợ/báo lỗi <i class="bi bi-exclamation-triangle"></i></a>
         </div>
       </div>
     </nav>
@@ -16,8 +21,8 @@
 
 <script>
 export default {
-  methods:{
-    refresh(){
+  methods: {
+    refresh() {
       window.location.reload();
     }
   }
@@ -25,7 +30,16 @@ export default {
 </script>
 
 <style lang="scss">
-.home{
-  cursor: pointer;
+
+.header {
+  .bg-ut {
+    color: #fafafa;
+    background: #005ab7 !important;
+  }
+
+  .home {
+    cursor: pointer;
+  }
+
 }
 </style>
