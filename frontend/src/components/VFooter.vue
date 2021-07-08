008c078c7549f63abb9d76eed244ada63708ec75
Loc Le <locle.isme@gmail.com>
2021-07-08 16:27:57 +0700

    add footer component

---
+++ frontend/src/components/VFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="mt-3 navbar navbar-light bg-light footer">
+    <div class="container-fluid justify-content-center">
+      <div class="copyright">
+        <i class="fa fa-copyright"></i>&nbsp;Made by
+        <a href="https://www.facebook.com/LocLe.isme/" target="_blank" class="text-decoration-none">tui</a>
+        &nbsp;<i class="bi bi-suit-heart-fill text-danger"></i>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "VFooter"
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+
+  .copyright {
+    font-size: 0.8em;
+  }
+}
+</style>
