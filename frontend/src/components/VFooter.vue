1ab773e33aa4bfc25d7ff295ec28ce7b3e9e7ba5
Loc Le <locle.isme@gmail.com>
2021-07-13 02:35:04 +0700

    update footer

--- frontend/src/components/VFooter.vue
+++ frontend/src/components/VFooter.vue
@@ -2,8 +2,8 @@
   <nav class="mt-3 navbar navbar-light bg-light footer">
     <div class="container-fluid justify-content-center">
       <div class="copyright">
-        <i class="fa fa-copyright"></i>&nbsp;Made by
-        <a href="https://github.com/locle-isme/calculator-GPA-HCMC-University-of-Transport" target="_blank" class="text-decoration-none">tui</a>
+        <i class="bi bi-github"></i>&nbsp;&nbsp;Github
+        <a href="https://github.com/locle-isme" target="_blank" class="text-decoration-none">tui</a>
         &nbsp;<i class="bi bi-suit-heart-fill text-danger"></i>
       </div>
     </div>
